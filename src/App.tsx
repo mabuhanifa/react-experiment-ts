b5d9f83e5122b425f37f2b706d42e9ac44975235
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-18 16:34:43 +0600

    CheckBox

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, type FormEvent } from "react";
+import CheckBox from "./components/CheckBox";
 import { useDispatch, useSelector } from "./contextAPI/hooks";
 
 export default function App() {
@@ -30,7 +31,7 @@ export default function App() {
         {
           todos && todos.map((todo) => (
             <div key={todo.id} className="flex items-center p-2">
-
+              <CheckBox todo={todo} />
               <h1 className={`text-2xl font-bold ${todo.isCompleted ? "line-through" : ""}`}>
                 {todo.title}
               </h1>
