131d6fc2887ce43d6d3654e3af9f1069fb6d20a4
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-18 17:06:06 +0600

    icon added

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,32 +1,44 @@
 import { FormEvent, useState } from "react";
+import { VscAdd } from "react-icons/vsc";
 import { useDispatch, useSelector } from "../contextAPI/hooks";
 import Todo from "./Todo";
 
 export default function Todos() {
+
     const [title, setTitle] = useState("");
+
     const dispatch = useDispatch();
+
     const todos = useSelector((s) => s.todos);
+
     const createTodo = (e: (FormEvent<HTMLFormElement | HTMLButtonElement>)) => {
         e.preventDefault();
         if (!title) return;
         dispatch({
             type: "ADD_TODO",
             payload: { title, id: todos.length + 1, isCompleted: false }
-        })
+        });
         setTitle("");
-    }
-    console.log(todos);
+    };
+
     return (
         <section className="p-10 ">
-            <form onSubmit={(e) => createTodo(e)}>
+
+            <form onSubmit={(e) => createTodo(e)} className="flex items-center gap-2">
                 <input
                     type="text"
                     name="title"
                     value={title}
                     className="bg-gray-300 rounded-md p-2"
                     onChange={(e) => setTitle(e.target.value)} />
-                <button type="submit" onSubmit={(e) => createTodo(e)}>Add</button>
+                <button
+                    type="submit"
+                    onSubmit={(e) => createTodo(e)}
+                    className="bg-blue-500 text-white rounded-md p-2.5">
+                    <VscAdd size={20} />
+                </button>
             </form>
+
             <div>
                 {
                     todos && todos.map((todo) => (
@@ -34,6 +46,7 @@ export default function Todos() {
                     ))
                 }
             </div>
+
         </section>
     )
 }
