c8ea09aed32d6d8f26d1c93f72cb0bc4b82430bb
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-23 17:33:49 +0600

    footer with links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,59 @@
-import { CiFacebook, CiLinkedin, CiTwitter } from "react-icons/ci";
-import { VscGithubAlt } from "react-icons/vsc";
+import {
+  BsFacebook,
+  BsGithub,
+  BsInstagram,
+  BsLinkedin,
+  BsStackOverflow,
+  BsTwitter,
+} from "react-icons/bs";
 
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-center text-white py-10">
       <div>© 2023 Copyright: Mohammed Abu Hanifa</div>
       <div className="flex justify-center items-center gap-x-10 py-5 mt-10">
-        <div className="icons">
-          <CiFacebook size={25} />
-        </div>
-        <div className="icons">
-          <CiTwitter size={25} />
-        </div>
-        <div className="icons">
-          <CiLinkedin size={25} />
-        </div>
-        <div className="icons">
-          <VscGithubAlt size={25} />
-        </div>
+        <a
+          href="https://github.com/mabuhanifa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/mohammed-abu-hanifa-4611b515b/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://stackoverflow.com/users/18106759/mohammed-abuhanifa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsStackOverflow />
+        </a>
+        <a
+          href="https://www.facebook.com/shourov.hanifa/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsFacebook />
+        </a>
+        <a
+          href="https://twitter.com/MohAbuhanifa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsTwitter />
+        </a>
+        <a
+          href="https://www.instagram.com/shourovahmd/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsInstagram />
+        </a>
       </div>
     </footer>
   );
